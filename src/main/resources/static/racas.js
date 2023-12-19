a3556f0640c184c05a1e093f2f260afe6b6fd666
Pedro Henrique <pedrosk99@outlook.com>
2023-12-18 21:15:09 -0300

    formatação do codigo

--- src/main/resources/static/racas.js
+++ src/main/resources/static/racas.js
@@ -1,9 +1,8 @@
-
 function adicionarRaca() {
-  const nomeRaca = document.getElementById("raca_name").value;
-  if (nomeRaca.trim() === "") {
-    alert("Por favor, insira o nome da raça.");
-    return;
+  const nomeRaca = document.getElementById('raca_name').value
+  if (nomeRaca.trim() === '') {
+    alert('Por favor, insira o nome da raça.')
+    return
   }
   fetch('http://localhost:8080/racas/', {
     method: 'POST',
@@ -12,63 +11,62 @@ function adicionarRaca() {
     },
     body: JSON.stringify({ nome: nomeRaca }),
   })
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("raca_name").value = "";
-    listarRacas();
-  })
-  .catch(error => console.error('Erro ao adicionar raça:', error));
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('raca_name').value = ''
+      listarRacas()
+    })
+    .catch((error) => console.error('Erro ao adicionar raça:', error))
 }
 
 function listarRacas() {
   fetch('http://localhost:8080/racas/')
-    .then(response => response.json())
-    .then(data => exibirRacas(data))
-    .catch(error => console.error('Erro ao obter raças:', error));
+    .then((response) => response.json())
+    .then((data) => exibirRacas(data))
+    .catch((error) => console.error('Erro ao obter raças:', error))
 }
 
 function excluirRaca(id) {
   fetch(`http://localhost:8080/racas/${id}`, {
     method: 'DELETE',
   })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Erro ao excluir raça.');
-    }
-    listarRacas();
-  })
-  .catch(error => console.error('Erro ao excluir raça:', error));
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Erro ao excluir raça.')
+      }
+      listarRacas()
+    })
+    .catch((error) => console.error('Erro ao excluir raça:', error))
 }
 
 function criarBotaoExcluir(id) {
-  const botaoExcluir = document.createElement('button');
-  botaoExcluir.textContent = 'Excluir';
-  botaoExcluir.className = 'btn btn-outline-danger';
-  botaoExcluir.addEventListener('click', () => excluirRaca(id));
+  const botaoExcluir = document.createElement('button')
+  botaoExcluir.textContent = 'Excluir'
+  botaoExcluir.className = 'btn btn-outline-danger'
+  botaoExcluir.addEventListener('click', () => excluirRaca(id))
 
-  return botaoExcluir;
+  return botaoExcluir
 }
 
 function exibirRacas(racas) {
-  const listaRacas = document.querySelector('.listaRacas');
+  const listaRacas = document.querySelector('.listaRacas')
 
-  listaRacas.innerHTML = "";
+  listaRacas.innerHTML = ''
 
-  racas.forEach(raca => {
-    const linha = document.createElement('tr');
-    const colunaNome = document.createElement('td');
-    colunaNome.textContent = raca.nome;
+  racas.forEach((raca) => {
+    const linha = document.createElement('tr')
+    const colunaNome = document.createElement('td')
+    colunaNome.textContent = raca.nome
 
-    const colunaAcoes = document.createElement('td');
-    colunaAcoes.appendChild(criarBotaoExcluir(raca.id));
+    const colunaAcoes = document.createElement('td')
+    colunaAcoes.appendChild(criarBotaoExcluir(raca.id))
 
-    linha.appendChild(colunaNome);
-    linha.appendChild(colunaAcoes);
-    listaRacas.appendChild(linha);
-  });
+    linha.appendChild(colunaNome)
+    linha.appendChild(colunaAcoes)
+    listaRacas.appendChild(linha)
+  })
 }
 
-
-document.addEventListener("DOMContentLoaded", function () {
-  listarRacas();
-});
+document.addEventListener('DOMContentLoaded', function () {
+  listarRacas()
+})
